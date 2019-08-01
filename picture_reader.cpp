b6549029f2983c7dcdb81737acbeb761b511aa8e
Alexander Varfolomeev <47699145+alexanderlvarfolomeev@users.noreply.github.com>
2019-08-01 17:49:53 +0300

    namespace Reader and get_images() function implemented

--- picture_reader.cpp
+++ picture_reader.cpp
@@ -4,9 +4,9 @@
 #include <utility>
 #include "picture_reader.hpp"
 
-picture_reader::picture_reader(const std::string location) : path_(location) {}
+Reader::picture_reader::picture_reader(const std::string location) : path_(location) {}
 
-void picture_reader::read(Picture& output, const std::string name) {
+void Reader::picture_reader::read(Picture& output, const std::string name) {
 	cv::Mat source_red = cv::imread(path_ + name + "_red.tif", 0);
 	cv::Mat source_green = cv::imread(path_ + name + "_green.tif", 0);
 	cv::Mat source_blue = cv::imread(path_ + name + "_blue.tif", 0);
@@ -16,7 +16,7 @@ void picture_reader::read(Picture& output, const std::string name) {
 	output = Picture(std::move(source_red), std::move(source_green), std::move(source_blue));
 }
 
-void picture_reader::correction(cv::Mat& source) {
+void Reader::picture_reader::correction(cv::Mat& source) {
 	double minimum, maximum;
 	cv::minMaxLoc(source, &minimum, &maximum);
 	std::cout << minimum << ' ' << maximum << '\n';
@@ -25,3 +25,14 @@ void picture_reader::correction(cv::Mat& source) {
 	source.convertTo(source, -1, alpha, beta);
 }
 
+ void Reader::get_images(std::vector<Picture>& pictures) {
+	picture_reader reader("../images\\");
+	std::vector<std::string> filenames;
+	cv::glob("../images/*_blue.tif", filenames, false);
+	pictures.resize(filenames.size());
+	for (int i = 0; i < filenames.size(); i++) {
+		size_t first_pos = filenames[i].find_last_of('\\') + 1;
+		size_t last_pos = filenames[i].find_last_of('_');
+		reader.read(pictures[i], filenames[i].substr(first_pos, last_pos - first_pos));
+	}
+}
--- picture_reader.hpp
+++ picture_reader.hpp
@@ -2,13 +2,18 @@
 
 #include <opencv2/imgproc.hpp>
 #include <string>
+#include <vector>
 #include "picture.hpp"
 
-class picture_reader {
-	const std::string path_;
-public:
-	explicit picture_reader(const std::string location);
-	void read(Picture& output, const std::string name);
-private:
-	static void correction(cv::Mat& source);
+namespace Reader {
+	class picture_reader {
+		const std::string path_;
+	public:
+		explicit picture_reader(const std::string location);
+		void read(Picture& output, const std::string name);
+	private:
+		static void correction(cv::Mat& source);
+	};
+
+	void get_images(std::vector <Picture>& pictures);
 };
